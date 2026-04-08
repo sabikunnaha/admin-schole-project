e39736d21d5dda05910006e169c34934b4a45e57
sabikunnaha <sabikurnahar65@gmail.com>
2026-04-08 11:57:10 +0600

    hide auth pages

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,38 +1,39 @@
 import { createBrowserRouter } from "react-router";
-import Login from "../pages/auth/Login.jsx";
-import Register from "../pages/auth/register/Register.jsx";
-import SchoolRegiForm from "../pages/auth/register/SchoolRegiForm.jsx";
-import AdmissionRegiForm from "../pages/auth/register/AdmissionRegiForm.jsx";
+// import Login from "../pages/auth/Login.jsx";
+// import Register from "../pages/auth/register/Register.jsx";
+// import SchoolRegiForm from "../pages/auth/register/SchoolRegiForm.jsx";
+// import AdmissionRegiForm from "../pages/auth/register/AdmissionRegiForm.jsx";
 import DashboardLayout from "../layouts/DashboardLayout.jsx";
 import SchoolDashboard from "../pages/school/SchoolDashboard.jsx";
 
 
 
 export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Login></Login>,
-  },
-  {
-    path: "register",
-    element: <Register></Register>,
-    children: [
-      {
-        index: true,
-        element: <SchoolRegiForm />,
-      },
-      {
-        path: "admission",
-        element: <AdmissionRegiForm />,
-      },
-    ],
-  },
+  // {
+  //   path: "/",
+  //   element: <Login></Login>,
+  // },
+  // {
+  //   path: "register",
+  //   element: <Register></Register>,
+  //   children: [
+  //     {
+  //       index: true,
+  //       element: <SchoolRegiForm />,
+  //     },
+  //     {
+  //       path: "admission",
+  //       element: <AdmissionRegiForm />,
+  //     },
+  //   ],
+  // },
 
 
   //school dashboard
+  // path: "/school/dashboard"
 
   {
-    path: "/school/dashboard",
+    path: "/",
     element: <DashboardLayout></DashboardLayout>,
     children: [
       {
